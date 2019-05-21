7a37b8efe96089c4ee98ce60bfc71a739799396b
Ken Bredemeyer <ken.bredemeyer@uwa.edu.au>
2019-05-21 10:37:01 +0800

    add roxygen comments. fix args.

--- R/PCCs.R
+++ R/PCCs.R
@@ -1,3 +1,11 @@
+#' Plot Person Characteristic Curves
+#'
+#' @param data_matrix Square data frame containing pairwise comparison data
+#'   converted to proportions.  Null comparisons must be represented as NA.
+#' @param betas Named vector of locations of performance ability, in logits.
+#' @param class_intervals The number of class intervals.
+#'
+#' @export
 plot_PCCs <- function(data_matrix, betas, class_intervals = 4) {
   stopifnot(class(data_matrix) == "data.frame" && class(betas) == "numeric")
 
@@ -45,24 +53,27 @@ plot_PCCs <- function(data_matrix, betas, class_intervals = 4) {
 	# store mean proportion and ability for each CI for all scripts
 	means_all <- vector("list", nrow(data_matrix))
 
-	par(ask = TRUE)
+	#par(ask = TRUE)
 	for (n in 1:nrow(data_matrix)) {
 	  # calculate mean proportions & mean locations
 	  compare <- data.table::data.table(comparison[[n]])
-	  means <- compare[ , .(mean_p = mean(Proportions), mean_a = mean(Locations)), by = "class interval"]
+	  #compare <- data.table::setDT(compare)
+	  #means <- compare[ , .(mean_p = mean(Proportions), mean_a = mean(Locations)), by = "class interval"]
+	  #data.table:::`[.data.table`(locations, j = .(start, end))
+	  means <- data.table:::`[.data.table`(compare, j = .(mean_p = mean(Proportions), mean_a = mean(Locations)), by = "class interval")
 	  means <- data.frame(means)   # with = FALSE  (normal subsetting)
 	  means_all[[n]] <- means
 
 	  #points for plotting
 	  for (i in 1:length(xx)) {
-	    y[i] <-  exp(xx[i] - b[n])/(1 + exp(xx[i] - b[n]))
+	    y[i] <-  exp(xx[i] - betas[n])/(1 + exp(xx[i] - betas[n]))
 	  }
 
 	  txt <- rle(pw[[n]])$lengths
 	  # plot window range = range of ability estimates (for the group of persons)
-	  plot(xx, y, xlim = c(min(b)-2, max(b)+1.2), ylim = c(0, 1.1), type = "l", xlab = "PCC for script ID",
+	  plot(xx, y, xlim = c(min(betas)-2, max(betas)+1.2), ylim = c(0, 1.1), type = "l", xlab = "PCC for script ID",
 	    ylab = "Expected Value", col="red", main = "Performance Characteristic Curve", sub = rownames(data_matrix)[n])
-	  axis(side=1, tick=TRUE, at= round(b[n], 3), padj=1, lwd.ticks = 2, col.ticks="red")
+	  axis(side=1, tick=TRUE, at= round(betas[n], 3), padj=1, lwd.ticks = 2, col.ticks="red")
 	  points(means[,3], means[,2], pch = 16)
 	  text(means[,3], means[,2], col = "cornflowerblue", labels = txt, pos = 3, offset = 0.5)
 	}
